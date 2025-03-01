fc8ec7e6053477d4163866322b69eb68ea490402
SumantaBhattacharya <sumanta2004@gmail.com>
2025-03-01 14:47:24 +0530

    feat: Implement StringBuilder class with immutability explanation

---
+++ StringBuilder.java
@@ -0,0 +1,8 @@
+// string is an immutable object
+// String object is created, its value cannot be changed. Any operation that appears to modify a String (such as concatenation, replacement, or substring) actually creates a new String object, leaving the original unchanged.
+
+public class StringBuilder{
+    public static void main(String[] args) {
+        
+    }
+}
