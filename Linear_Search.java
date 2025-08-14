30fc2777786cc8d9644b93721a380c130b3fc849
SumantaBhattacharya <sumanta2004@gmail.com>
2025-08-14 10:26:47 +0530

    Enhance documentation for Linear Search algorithm with detailed explanation of its workings, time complexity, advantages, and drawbacks.

--- Linear_Search.java
+++ Linear_Search.java
@@ -81,3 +81,48 @@ i (Start Index) 	i + 6 (End Index)	Extracted Substring	Matches "genius"?
 2	8	            "erybod"	                ❌ No
 10	16	            "genius"	                ✅ Yes!
 */
+
+/*
+ Linear Search
+
+Linear Search is defined as a sequential search algorithm that starts at one end and goes through each element of a list until the desired element is found; otherwise, the search continues till the end of the data set.
+
+How Does Linear Search Algorithm Work?
+
+Every element is considered as a potential match for the key and checked for the same.
+
+If any element is found equal to the key, the search is successful and the index of that element is returned.
+
+If no element is found equal to the key, the search yields “No match found”.
+
+Time Complexity:
+
+Best Case: In the best case, the key might be present at the first index. So the best case complexity is O(1).
+
+Worst Case: In the worst case, the key might be present at the last index, i.e., opposite to the end from which the search has started in the list. So the worst-case complexity is O(N), where N is the size of the list.
+
+Average Case: O(N)
+
+Complexity Analysis of Linear Search:
+
+Auxiliary Space: O(1) as except for the variable to iterate through the list, no other variable is used.
+
+Advantages of Linear Search:
+
+Linear search can be used irrespective of whether the array is sorted or not. It can be used on arrays of any data type.
+
+Does not require any additional memory.
+
+It is a well-suited algorithm for small datasets.
+
+Drawbacks of Linear Search:
+
+Linear search has a time complexity of O(N), which in turn makes it slow for large datasets.
+
+Not suitable for large arrays.
+
+Example Problem:
+
+Q. Find whether a number exists in an array or not.
+File path: C:\Users\SUDIP BHATTACHARYA\Desktop\JAVA-DSA\Number_exists_in_an_array.java
+ */
