8b76735cf7f850aea9c29424c2052f23bd3895ee
SumantaBhattacharya <sumanta2004@gmail.com>
2025-09-08 09:31:14 +0530

    Selection_Sort

---
+++ Selection_Sort_Dsc.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Selection_Sort_Dsc {
+    
+    public static void main(String[] args) {
+        
+        
+        int[] arr = {
+                69,
+                68,
+                101,
+                180,
+                440,
+                480,
+                781
+        };
+
+
+        int[] result2 = Selection_Sort(arr);
+        System.err.println("Selection_Sort"+Arrays.toString(result2));
+
+    }
+
+    private static int[] Selection_Sort(int[] arr) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'Selection_Sort'");
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            
+            int smallestIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                
+                if (arr[smallestIndex] < arr[j] ) {
+                    
+                    smallestIndex = j;// each time the inner loops runs we are getting the smallest element
+
+                }
+
+            }
+
+            // here we are swapping the elements
+            int temp = arr[smallestIndex];
+            arr[smallestIndex] = arr[i]; // put the "i-th element" into that smallest's place from the left to right
+            arr[i] = temp;
+
+        }
+
+        return arr;
+
+    }
+
+}
+
