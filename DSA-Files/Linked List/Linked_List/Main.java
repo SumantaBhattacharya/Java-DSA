e5c6f3a0629eb82dfe6e7bfb222f16bd1f35fa6f
SumantaBhattacharya <sumanta2004@gmail.com>
2025-10-03 10:01:04 +0530

    learning from https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/lectures/18-linkedlist/code/src/com/kunal/LL.java

--- DSA-Files/Linked List/Linked_List/Main.java
+++ DSA-Files/Linked List/Linked_List/Main.java
@@ -2,6 +2,7 @@ package Linked_List;
 
 // import java.util.LinkedList;
 
+// https://github.com/kunal-kushwaha/DSA-Bootcamp-Java/blob/main/lectures/18-linkedlist/code/src/com/kunal/LL.java
 public class Main {
     public static void main(String[] args) {
         // LinkedList<Integer> list = new LinkedList<>();
@@ -12,8 +13,12 @@ public class Main {
         list.insertFirst(69);
         list.insertFirst(68);
         list.insertFirst(440);
-        list.insertFirst(480);
-        
+        list.insertLast(480);
+
+        list.print();
+
+        // print the selected index elelement 
+        list.insert(40, 2);
 
         list.print();
 
