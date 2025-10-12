9461f6feca3909be6746bc50018497f26bf7f91c
SumantaBhattacharya <sumanta2004@gmail.com>
2025-10-12 07:43:25 +0530

    JECA_2023_Q6

---
+++ JECA_P/JECA_2023_Q6.java
@@ -0,0 +1,8 @@
+package JECA_P;
+
+public class JECA_2023_Q6 {
+    public static void main(String[] args) {
+        char M = 'M'; // Single quotes for char in Java
+        System.out.printf("%d, %c%n", (int) M, M);
+    }
+}
