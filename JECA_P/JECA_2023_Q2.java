7a8ff5d12675e2f284bfeccc542625a183cb9059
SumantaBhattacharya <sumanta2004@gmail.com>
2025-10-12 07:42:39 +0530

    JECA_2023_Q2

---
+++ JECA_P/JECA_2023_Q2.java
@@ -0,0 +1,10 @@
+package JECA_P;
+
+public class JECA_2023_Q2 {
+    public static void main(String[] args) {
+        int a = -7;
+        float b;
+        b = a++;
+         System.out.printf("%d, %f %n", a, ++b); 
+    }
+}
